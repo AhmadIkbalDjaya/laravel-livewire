5e16a3e3586371bf2c4376f48e0bc476a0f173a9
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-04-04 12:34:29 +0700

    create data

--- app/Http/Livewire/UserCreate.php
+++ app/Http/Livewire/UserCreate.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
 
 class UserCreate extends Component
 {
+
+    public $name;
+    public $email;
+    public $password;
+
     public function render()
     {
         return view('livewire.user-create');
+    }
+
+    public function store()
+    {
+        $validated = $this->validate([
+            "name" => "required|string|min:3",
+            "email" => "required|email|unique:users,email",
+            "password" => "required|min:8",
+        ]);
+        $validated["password"] = Hash::make($validated["password"]);
+        User::create($validated);
+        $this->name = null;
+        $this->email = null;
+        $this->password = null;
+        session()->flash("success", "User Berhasil Ditambahkan");
+
     }
 }
--- resources/views/livewire/user-create.blade.php
+++ resources/views/livewire/user-create.blade.php
@@ -3,19 +3,42 @@
     <div class="card">
         <div class="card-header">Form</div>
         <div class="card-body">
-            <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input type="text" wire:model="name" class="form-control" id="name" placeholder="Name">
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">email</label>
-                <input type="email" wire:model="email" class="form-control" id="email" placeholder="email">
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">password</label>
-                <input type="password" wire:model="password" class="form-control" id="password" placeholder="password">
-            </div>
-            <button class="btn btn-primary">Submit</button>
+            @if (session()->has('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            <form wire:submit.prevent="store">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input type="text" wire:model="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Name">
+                    @error('name')
+                    <div id="" class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">email</label>
+                    <input type="email" wire:model="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="Email">
+                    @error('email')
+                    <div id="" class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">password</label>
+                    <input type="password" wire:model="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password">
+                    @error('password')
+                    <div id="" class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+                <button class="btn btn-primary" type="submit">Submit</button>
+            </form>
         </div>
     </div>
 </div>
